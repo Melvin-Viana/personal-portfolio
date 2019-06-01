fabe0a84f7a6875df9e5bb800c6dc83e68046fd9
Melvin Viana <mjviana057@gmail.com>
2019-06-01 00:03:30 -0700

    Typed.js element implent

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
 //  TODO: Display nav when bar icon is clicked
 
 // TODO: Onscroll set nav to fixed position
+// =============================================
+// Typed.js
+// Can also be included with a regular script tag
+let options = {
+  strings: ["create websites 💻 and apps 📱. ", "am a Full Stack enthusiast. 🤔 ","love learning about new technology.", "solve problems."],
+  typeSpeed: 30,
+  backSpeed:30,
+  onComplete:function(){
+      const typed=document.querySelector('.typed');
+      typed.style.background="#1f2833";
+      typed.style.color="#fff"
+  }
+}
+
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    //Iniate typed object on dom load.
+    let typed = new Typed(".typed", options);
+
+});
