3f6935c407250693b21732b6e4d70d0d49483223
Melvin Viana <mjviana057@gmail.com>
2019-06-02 22:27:15 -0700

    Add fadeIn after completed typed.js

--- js/app.js
+++ js/app.js
@@ -1,20 +1,24 @@
 //  TODO: Display nav when bar icon is clicked
 
 // TODO: Onscroll set nav to fixed position
+
+const downArrow = document.querySelector('.down');
+
 // =============================================
 // Typed.js
 // Can also be included with a regular script tag
 let options = {
-  strings: ["create websites 💻 and apps 📱. ", "am a Full Stack enthusiast. 🤔 ","love learning about new technology.", "solve very challenging problems."],
+  strings: ["create websites 💻 and apps 📱. ", "am a Full Stack enthusiast. 🤔 ","love learning about new technology.", "solve problems."],
   typeSpeed: 30,
-  backSpeed:30,
+  backSpeed:25,
   onComplete:function(){
       const typed=document.querySelector('.typed');
       typed.style.background="#1f2833";
       typed.style.color="#fff"
-  }
+      setTimeout(()=>{downArrow.style.opacity="1";
+      downArrow.firstElementChild.classList.add('fadeOutDown');},500);
 }
-
+}
 
 document.addEventListener('DOMContentLoaded', ()=>{
     //Iniate typed object on dom load.
