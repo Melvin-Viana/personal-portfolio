876bf1e491548eb0a7af448accc4c620ea22c564
Melvin Viana <mjviana057@gmail.com>
2019-06-05 18:06:36 -0700

    Ad animations to header text

--- js/app.js
+++ js/app.js
@@ -14,8 +14,9 @@ const downArrow = document.querySelector('.down');
 // Can also be included with a regular script tag
 let options = {
     strings: ["create websites 💻 and apps 📱. ", "am a Full Stack enthusiast. 🤔 ", "love learning about new technology.", "solve problems."],
-    typeSpeed: 30,
-    backSpeed: 25,
+    typeSpeed: 25,
+    backSpeed: 20,
+    startDelay:1500,
     onComplete: function () {
         const typed = document.querySelector('.typed');
         typed.style.background = "#1f2833";
